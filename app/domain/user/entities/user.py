5cd5988498b3361e9d9b2857252b6b345a887435
Leonardo Magalhães <leonardo@biomainvestimentos.com>
2023-04-10 19:10:35 -0300

    "Add": adicionando coluna role na tabela usuario

--- app/domain/user/entities/user.py
+++ app/domain/user/entities/user.py
@@ -1,11 +1,36 @@
+from enum import Enum
 import sqlalchemy as sa
+from sqlalchemy.types import VARCHAR, TypeDecorator
 from app.core.entities.base import Base
 from app.core.entities.audits import Audits
+
+
+class UserRoleList(str, Enum):
+    ADMIN = "ADMIN"
+    NORMAL_USER = "NORMAL_USER"
+    SPECIALIST = "SPECIALIST"
+
+
+class RolesDB(TypeDecorator):
+    impl = VARCHAR(255)
+
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, UserRoleList):
+            return value.value
+        elif isinstance(value, str):
+            return value
+        raise ValueError("Invalid value for RolesDB")
+
+    def process_result_value(self, value, dialect):
+        return UserRoleList(value)
 
 
 class User(Base, Audits):
     id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     email = sa.Column(sa.String)
     name = sa.Column(sa.String)
+    role = sa.Column(RolesDB(UserRoleList))
     telephone = sa.Column(sa.String, nullable=True)
     password = sa.Column(sa.String)
